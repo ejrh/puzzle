57d59bada4504133b954280e7056c587c23c22fb
ejrh <ejrh@users.noreply.github.com>
2026-04-05 04:56:16 +0000

    deploy: 36bb46e232e7f8ac183cf5d37e7ff9b79c98ca57

---
+++ gui/puzzle.d.ts
@@ -0,0 +1,52 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly main: (a: number, b: number) => number;
+    readonly __wasm_bindgen_func_elem_142122: (a: number, b: number, c: number, d: number) => void;
+    readonly __wasm_bindgen_func_elem_141457: (a: number, b: number, c: number, d: number) => void;
+    readonly __wasm_bindgen_func_elem_27312: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_3: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_4: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_5: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_6: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_7: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_8: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_9: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_10: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_11: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_27312_12: (a: number, b: number, c: number) => void;
+    readonly __wasm_bindgen_func_elem_155724: (a: number, b: number) => void;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_export3: (a: number) => void;
+    readonly __wbindgen_export4: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_export5: (a: number, b: number) => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ gui/puzzle_bg.wasm.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_142122: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_141457: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_27312: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_3: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_4: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_10: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_11: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_27312_12: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_155724: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_export5: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
